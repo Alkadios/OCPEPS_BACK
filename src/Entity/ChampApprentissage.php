44b5b0d0c53f35e9495f24c8042715f16160897a
Lucas Delefortrie <91888260+LucasDelefortrie@users.noreply.github.com>
2022-03-15 10:57:49 +0100

    feat: get les apsa de champ apprentissage

--- src/Entity/ChampApprentissage.php
+++ src/Entity/ChampApprentissage.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use App\Controller\ApsaChampApprentissageController;
 use App\Repository\ChampApprentissageRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -13,6 +14,16 @@ use Symfony\Component\Serializer\Annotation\Groups;
  * @ORM\Entity(repositoryClass=ChampApprentissageRepository::class)
  */
 #[ApiResource(
+    collectionOperations: [
+        'getApsa' => [
+            'method' => 'GET',
+            'path' => '/posts/getApsa',
+            'controller' => ApsaChampApprentissageController::class,
+            'openapi_context' => [
+                'summary' => 'Get les APSA de ChampApprentissage'
+]
+        ]
+    ],
     normalizationContext: [
         'groups' => ['read:apsa', 'read:champapprentissage'],
     ]
