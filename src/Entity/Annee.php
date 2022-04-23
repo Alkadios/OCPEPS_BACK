88159a2c6ee04191f1b33d8d4ea79c53a26958e2
Quentin <quentin-ol@hotmail.com>
2022-04-23 11:20:47 +0200

    feat: add filter annee en cours

--- src/Entity/Annee.php
+++ src/Entity/Annee.php
@@ -2,17 +2,18 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
-use App\Repository\AnneeRepository;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=AnneeRepository::class)
  */
 #[ApiResource()]
+#[ApiFilter(BooleanFilter::class, properties: ['en_cours' => 'exact'])]
 class Annee
 {
     /**
